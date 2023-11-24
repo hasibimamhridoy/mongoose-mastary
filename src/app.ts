c1e0b77f41c45b2f49174262ced5db3bcd93efa5
hasibimamhridoy <hasibimamhridoy@gmail.com>
2023-11-24 19:56:05 +0600

    api nameing convension fixed

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded())
 app.use(cookieParser())
-app.use('/api/v1/users', UserRoutes.userRouter)
+app.use('/api/users', UserRoutes.userRouter)
 
 
 //global
